e308d3319fc1d067373f6348f22b8bf72503fd27
oshaleyko59 <oshaleyko@yahoo.com>
2023-02-25 22:47:15 -0600

    task #1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+//find gallery element
+const galleryContainer = document.querySelector(".gallery");
+
+//render gallery content
+galleryContainer.insertAdjacentHTML(
+	"beforeend",
+	createGalleryMarkup(galleryItems)
+);
+
+//initialize SimpleLightBox with options for source of caption and caption delay ms
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
+
+//functions below can be moved to external module
+//and createGalleryMarkup can be exported from there
+//** creates markup for gallery content from galleryItems array
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(createGalleryItem).join('');
+}
+
+//** creates markup for a gallery item from given template
+function createGalleryItem(element) {
+  return `<a class="gallery__item" href="${element.original}">
+						<img
+							class="gallery__image"
+							src="${element.preview}"
+							alt="${element.description}"
+						/>
+					</a>`;
+}
+
